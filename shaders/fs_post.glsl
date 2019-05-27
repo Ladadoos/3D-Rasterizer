d3167df194ad8a954f801a8839547d39f28f18dc
Dion <dionkamerman@gmail.com>
2019-05-27 21:34:52 +0200

    Added init files

---
+++ shaders/fs_post.glsl
@@ -0,0 +1,19 @@
+#version 330
+
+// shader input
+in vec2 P;						// fragment position in screen space
+in vec2 uv;						// interpolated texture coordinates
+uniform sampler2D pixels;		// input texture (1st pass render target)
+
+// shader output
+out vec3 outputColor;
+
+void main()
+{
+	// retrieve input pixel
+	outputColor = texture( pixels, uv ).rgb;
+	// apply dummy postprocessing effect
+	outputColor *= sin( P.y * 400 ) * 0.15f + 0.85f;
+}
+
+// EOF
